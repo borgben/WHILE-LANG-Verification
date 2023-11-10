d90934e89a051e65987e27c7fc3df67bbb37a9fc
borgben <borgben@ben-kali-laptop.developement-kali>
2023-11-10 01:42:27 +0100

    Code Compiles, Problem with GetPostCondition

--- problem_3/main.ml
+++ problem_3/main.ml
@@ -1,7 +1,14 @@
 
 open Implang;;
+open Z3;;
+open Verifier;;
+
 
 let lexbuf = Lexing.from_channel stdin ;;
 
 let result = Parser.main Lexer.token lexbuf;;
-print_string (stmtToStr result);;
+let ctx =Z3.mk_context [("model", "true"); ("proof", "false")];;
+let post_condition = exprToPredicate (ctx) (Verifier.getPostCondition result);;
+let verify_expr = Verifier.stmtToPredicate ctx result post_condition;;
+
+print_string (z3ExprString verify_expr);;
--- problem_3/verifier.ml
+++ problem_3/verifier.ml
@@ -1,6 +1,3 @@
-#!/usr/bin/env ocaml
-#use "topfind";;
-#require "z3";;
 open Implang
 open Z3
 
@@ -8,32 +5,45 @@ open Z3
   match ast with
   |  Seq(stmt',stmt'') ->  *)
 
-type vc = VC of stmt * Z3.Expr.expr
+let z3ExprString (expr:Z3.Expr.expr):string = (Z3.Expr.to_string expr)
 
-let rec curryBinaryZ3Fn (fn:context->Z3.Expr.expr->Z3.Expr.expr>Z3.Expr.expr) (ctx:context) (expr_list:expr list) =
+let rec curryBinaryZ3Fn (fn:context->Z3.Expr.expr->Z3.Expr.expr->Z3.Expr.expr) (ctx:context) (expr_list:Z3.Expr.expr list) =
   match expr_list with 
     []     -> failwith "Invalid Expression List as input to Binary Expression Function"
-   | hd:hd' -> fn ctx hd hd'
-   | hd:tl  -> fn ctx hd (curryBinaryZ3Fn fn ctx tl) 
+   | [hd;hd'] -> fn ctx hd hd'
+   | hd::tl  -> fn ctx hd (curryBinaryZ3Fn fn ctx tl) 
   
 let rec getPreCondition (stmt:stmt) =
   match stmt with
    Seq(stmt', _) -> 
-    match stmt' with
+    (match stmt' with
       Pre(expr) -> expr
-    | _         -> failwith "Program doesn't contain a pre-condition!"
+    | _         -> failwith "Program doesn't contain a pre-condition!")
   | Pre(expr)   -> expr
   | _ -> failwith "Program doesn't contain a pre-condition!"
 
 let rec getPostCondition (stmt:stmt) =
     match stmt with
      Seq(_, stmt'') -> 
-      match stmt'' with
+      (match stmt'' with
         Post(expr) -> expr
-      | _          -> getPostCondition stmt''
+      | _          -> getPostCondition stmt'')
     | Post(expr)   -> expr
     | _ -> failwith "Program doesn't contain a post-condition!"
 
+let rec unopToPredicate (ctx:context) (unary_operator:unop) = 
+    match unary_operator with 
+      Not -> Z3.Boolean.mk_not ctx 
+
+let binopToPredicate (ctx:context) (binary_operator:binop) = 
+    match binary_operator with 
+          Plus  ->  Z3.Arithmetic.mk_add ctx
+        | Minus ->  Z3.Arithmetic.mk_sub ctx
+        | Times ->  Z3.Arithmetic.mk_mul ctx
+        | Lt    ->  (curryBinaryZ3Fn Z3.Arithmetic.mk_lt  ctx)
+        | And   ->  Z3.Boolean.mk_and ctx
+        | Or    ->  Z3.Boolean.mk_or ctx
+        | Eq    ->  (curryBinaryZ3Fn Z3.Boolean.mk_eq ctx)
 
 let rec exprToPredicate (ctx:context) (expr:expr) =
   match expr with 
@@ -41,26 +51,22 @@ let rec exprToPredicate (ctx:context) (expr:expr) =
    | Var(identifier) -> Z3.Arithmetic.Integer.mk_const_s ctx identifier
    | Unary(unary_op,expr') -> (unopToPredicate ctx unary_op) (exprToPredicate ctx expr')
    | Binary(binary_op,expr',expr'') -> (binopToPredicate ctx binary_op) [(exprToPredicate ctx expr');(exprToPredicate ctx expr'')]
-   | _ -> failwith ("unsupported expression " ^ (exprToStr))
+   | _ -> failwith ("unsupported expression " ^ (exprToStr expr))
 
-let rec unopToPredicate (ctx:context) (unary_operator:unop) = 
-  match unary_operator with 
-    Not -> Z3.Boolean.mk_not ctx 
 
-let binopToPredicate (ctx:context) (binary_operator:unop) = 
-  match binary_operator with 
-    Plus  ->  Z3.Arithmetic.Integer.mk_add ctx
-  | Minus ->  Z3.Arithmetic.Integer.mk_sub ctx
-  | Times ->  Z3.Arithmetic.Integer.mk_mul ctx
-  | Lt    ->  (curryBinaryZ3Fn Z3.Arithmetic.Integer.mk_lt  ctx)
-  | And   ->  (curryBinaryZ3Fn Z3.Boolean.mk_and ctx)
-  | Or    ->  (curryBinaryZ3Fn Z3.Boolean.mk_or ctx) 
-  | Eq    ->  (curryBinaryZ3Fn Z3.Boolean.mk_eq ctx)
 
-let stmtToPredicate (ctx:context) (stmt:stmt) = 
+let rec stmtToPredicate (ctx:Z3.context) (stmt:stmt) (expr:Z3.Expr.expr) = 
   match stmt with 
-    Skip -> ctx
-  | Post(_) -> ctx
-  | Pre(_)  -> ctx
-  | Assign(identifier, expr) ->  Z
+    Skip    -> expr
+  | Post(_) -> expr
+  | Pre(_)  -> expr
+  | Assign(identifier, expr') ->  Z3.Expr.substitute expr [(Z3.Arithmetic.Integer.mk_const_s ctx identifier)] [(exprToPredicate ctx expr')]
+  | Seq(stmt',stmt'')         ->  stmtToPredicate ctx stmt' (stmtToPredicate ctx stmt'' expr)
+  | Ifthen(condition,if_stmt,else_stmt) -> 
+      let condition_expr = exprToPredicate ctx condition in
+      let if_expr   = (Z3.Boolean.mk_and ctx [condition_expr;(stmtToPredicate ctx if_stmt expr)]) in 
+      let else_expr = (Z3.Boolean.mk_and ctx [(Z3.Boolean.mk_not ctx condition_expr);(stmtToPredicate ctx else_stmt expr)]) in 
+      (Z3.Boolean.mk_or ctx [if_expr;else_expr])
+  | _ -> failwith ("Unsupported statement"^(stmtToStr stmt))
+
 
