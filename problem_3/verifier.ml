4ab4d4b4490d37e66c84e9047089769d8efa0928
borgben <borgben@ben-kali-laptop.developement-kali>
2023-11-11 20:25:33 +0100

    Scan for Modified Variables Function

--- problem_3/verifier.ml
+++ problem_3/verifier.ml
@@ -3,13 +3,13 @@ open Z3
 
 let z3ExprString (expr:Z3.Expr.expr):string = (Z3.Expr.to_string expr)
 
-let rec curryBinaryZ3Fn (fn:Z3.context->Z3.Expr.expr->Z3.Expr.expr->Z3.Expr.expr) (ctx:Z3.context) (expr_list:Z3.Expr.expr list) =
+let rec curryBinaryZ3Fn (fn:Z3.context->Z3.Expr.expr->Z3.Expr.expr->Z3.Expr.expr) (ctx:Z3.context) (expr_list:Z3.Expr.expr list):Z3.Expr.expr =
   match expr_list with 
-    []     -> failwith "Invalid Expression List as input to Binary Expression Function"
+    []        -> failwith "Invalid Expression List as input to Binary Expression Function"
    | [hd;hd'] -> fn ctx hd hd'
-   | hd::tl  -> fn ctx hd (curryBinaryZ3Fn fn ctx tl) 
+   | hd::tl   -> fn ctx hd (curryBinaryZ3Fn fn ctx tl) 
   
-let rec getPreCondition (stmt:stmt) =
+let rec getPreCondition (stmt:stmt):expr =
   match stmt with
    Seq(stmt', _) -> 
     (match stmt' with
@@ -18,7 +18,7 @@ let rec getPreCondition (stmt:stmt) =
   | Pre(expr)   -> expr
   | _ -> failwith "Program doesn't contain a pre-condition!"
 
-let rec getPostCondition (stmt:stmt) =
+let rec getPostCondition (stmt:stmt):expr option=
     match stmt with
      Seq(stmt', stmt'') -> 
       (match stmt'' with
@@ -32,11 +32,22 @@ let rec getPostCondition (stmt:stmt) =
     | Post(expr)   -> Some(expr)
     | _ -> None
 
-let rec unopToPredicate (ctx:Z3.context) (unary_operator:unop) = 
+let rec scanForModifiedVariables (stmt:stmt): string list = 
+  match stmt with 
+    Seq(stmt',stmt'') -> (scanForModifiedVariables stmt')@(scanForModifiedVariables stmt'')
+  | Skip              -> []
+  | Post(_)           -> []
+  | Pre(_)            -> []
+  | Assign(identifier,expr) -> [identifier]
+  | Ifthen(_,if_stmt,else_stmt) -> (scanForModifiedVariables if_stmt)@(scanForModifiedVariables else_stmt)
+  | Whileloop(_,_,stmt')        -> scanForModifiedVariables stmt'
+  | _ -> failwith ("Unsupported statement"^(stmtToStr stmt))
+
+let rec unopToPredicate (ctx:Z3.context) (unary_operator:unop):Z3.Expr.expr -> Z3.Expr.expr = 
     match unary_operator with 
       Not -> Z3.Boolean.mk_not ctx 
 
-let binopToPredicate (ctx:Z3.context) (binary_operator:binop) = 
+let binopToPredicate (ctx:Z3.context) (binary_operator:binop):Z3.Expr.expr list -> Z3.Expr.expr = 
     match binary_operator with 
           Plus  ->  Z3.Arithmetic.mk_add ctx
         | Minus ->  Z3.Arithmetic.mk_sub ctx
@@ -46,7 +57,7 @@ let binopToPredicate (ctx:Z3.context) (binary_operator:binop) =
         | Or    ->  Z3.Boolean.mk_or ctx
         | Eq    ->  (curryBinaryZ3Fn Z3.Boolean.mk_eq ctx)
 
-let rec exprToPredicate (ctx:Z3.context) (expr:expr) =
+let rec exprToPredicate (ctx:Z3.context) (expr:expr):Z3.Expr.expr =
   match expr with 
      Num(integer) -> Z3.Arithmetic.Integer.mk_numeral_i ctx integer
    | Var(identifier) -> Z3.Arithmetic.Integer.mk_const_s ctx identifier
@@ -54,7 +65,7 @@ let rec exprToPredicate (ctx:Z3.context) (expr:expr) =
    | Binary(binary_op,expr',expr'') -> (binopToPredicate ctx binary_op) [(exprToPredicate ctx expr');(exprToPredicate ctx expr'')]
    | _ -> failwith ("unsupported expression " ^ (exprToStr expr))
 
-let rec stmtToPredicate (ctx:Z3.context) (stmt:stmt) (expr:Z3.Expr.expr) = 
+let rec stmtToPredicate (ctx:Z3.context) (stmt:stmt) (expr:Z3.Expr.expr):Z3.Expr.expr = 
   match stmt with 
     Skip    -> expr
   | Post(_) -> expr
@@ -66,4 +77,8 @@ let rec stmtToPredicate (ctx:Z3.context) (stmt:stmt) (expr:Z3.Expr.expr) =
       let if_expr   = (Z3.Boolean.mk_and ctx [condition_expr;(stmtToPredicate ctx if_stmt expr)]) in 
       let else_expr = (Z3.Boolean.mk_and ctx [(Z3.Boolean.mk_not ctx condition_expr);(stmtToPredicate ctx else_stmt expr)]) in 
       (Z3.Boolean.mk_or ctx [if_expr;else_expr])
+  | Whileloop(condition,invariant, stmt') -> (
+      let modified_variables = scanForModifiedVariables stmt' in
+      (* 2. Build Expression. *)
+    )
   | _ -> failwith ("Unsupported statement"^(stmtToStr stmt))
